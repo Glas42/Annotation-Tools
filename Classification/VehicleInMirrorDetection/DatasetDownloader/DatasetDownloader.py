0f8749a7c69876d3feee842308a066bab140cb20
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-07-08 17:42:35 +0200

    fixes

--- Classification/VehicleInMirrorDetection/DatasetDownloader/DatasetDownloader.py
+++ Classification/VehicleInMirrorDetection/DatasetDownloader/DatasetDownloader.py
@@ -8,7 +8,6 @@ if not os.path.exists(f"{os.path.dirname(__file__)}/Dataset"):
 
 index = len(os.listdir(f"{os.path.dirname(__file__)}/Dataset"))
 
-stop = False
 thread_count = 0
 max_threads = 100
 
@@ -16,19 +15,18 @@ print("Starting from index", index)
 
 def download(image_number):
     global thread_count
-    global stop
     downloaded = False
     while not downloaded:
         try:
             r = requests.get(f"https://filebrowser.tumppi066.fi/api/public/dl/Uw850Xow/{image_number}.png?inline=true")
             if r.status_code == 404:
-                stop = True
-                return
+                time.sleep(60)
+                continue
             with open(f"{os.path.dirname(__file__)}/Dataset/{image_number}.png", "wb") as f:
                 f.write(r.content)
             downloaded = True
         except:
-            pass
+            time.sleep(60)
     thread_count -= 1
 
 for i in range(index):
@@ -45,13 +43,10 @@ for i in range(index):
 print(f"\r                                                                                           ", end="")
 
 while True:
-    while stop == False:
-        if thread_count < max_threads:
-            threading.Thread(target=download, args=(index,), daemon=True).start()
-            thread_count += 1
-            index += 1
-        else:
-            time.sleep(0.01)
+    if thread_count < max_threads:
+        threading.Thread(target=download, args=(index,), daemon=True).start()
+        thread_count += 1
+        index += 1
         print(f"\rDownloaded {index} files", end="")
-    time.sleep(60)
-    stop = False
+    else:
+        time.sleep(0.01)
