cf5effb9c762a5b63d6fca0b5111d86103a6a222
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-07-12 22:52:04 +0200

    fix reading grayscale images as bgr/rgb image

--- Classification/VehicleInMirrorDetection/DatasetDownloader/DatasetChecker.py
+++ Classification/VehicleInMirrorDetection/DatasetDownloader/DatasetChecker.py
@@ -6,8 +6,8 @@ if not os.path.exists(f"{os.path.dirname(__file__)}/Dataset"):
 
 for file in os.listdir(f"{os.path.dirname(__file__)}/Dataset"):
     img = None
-    img = cv2.imread(f"{os.path.dirname(__file__)}/Dataset/{file}")
+    img = cv2.imread(f"{os.path.dirname(__file__)}/Dataset/{file}", cv2.IMREAD_UNCHANGED)
     if img is None:
         os.remove(f"{os.path.dirname(__file__)}/Dataset/{file}")
-    elif img.shape[2] == 3:
+    elif len(img.shape) == 3:
         cv2.imwrite(f"{os.path.dirname(__file__)}/Dataset/{file}", cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
